6ef002099c4eb0a809205b3d7ea77817d65c759a
Janis <janisbunkss@inbox.lv>
2022-07-26 21:03:51 +0300

    Update index.js
    Komentēts background

--- index.js
+++ index.js
@@ -33,15 +33,15 @@ function favouriteMovieGenre(font) {
     }
 }
 
-function favouriteMode(mode) {
-    if (mode === "light" || !mode) {
-        setProp('--background', "var(--light)");
-        setProp('--text', "var(--dark)");
-    } else if (mode === "dark") {
-        setProp('--background', "var(--dark)");
-        setProp('--text', "var(--light)");
-    }
-}
+// function favouriteMode(mode) {
+//     if (mode === "light" || !mode) {
+//         setProp('--background', "var(--light)");
+//         setProp('--text', "var(--dark)");
+//     } else if (mode === "dark") {
+//         setProp('--background', "var(--dark)");
+//         setProp('--text', "var(--light)");
+//     }
+// }
 
 function favouriteFruit(theme) {
     if (theme === "pastel") {
